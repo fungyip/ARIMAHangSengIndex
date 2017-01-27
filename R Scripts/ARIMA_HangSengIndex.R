697a2706a13b7657864474961c5e148b5c21411e
fungyip <fung.yip@gmail.com>
2017-01-27 09:55:16 +0800

    Update R scripts

--- R Scripts/ARIMA_HangSengIndex.R
+++ R Scripts/ARIMA_HangSengIndex.R
@@ -3,6 +3,8 @@
 # install.packages("forecast")
 
 # library(devtools)
+
+#Data Extraction from Quandl - Hang Seng Index
 library(Quandl)
 HSI = Quandl("YAHOO/INDEX_HSI" , start_date="1976-01-01",end_date="2016-12-31" , collapse="monthly",type= "ts")
 plot(HSI)
@@ -10,21 +12,28 @@ plot(HSI)
 HSI_data <- as.data.frame(HSI)
 HSI_data_Close <- HSI_data$Close
 
-#initialize the time
+#Add Time dimension
+library(dse)
 starttime=c(1976,1)
 endtime=c(2016,12)
-#monthly data
-freq=12
+freq=12 #monthly data
 
-HSI_ts <- ts(HSI_data_Close, start = starttime, end = endtime, frequency = freq)
-plot(HSI_ts)
-
+HSI_Close <- ts(HSI_data_Close, start = starttime, end = endtime, frequency = freq)
+plot(HSI_Close)
 
 library(forecast)
-fit <- auto.arima(HSI_ts)
+fit <- auto.arima(HSI_Close)
+summary(fit)
 fitted(fit)
-forecast(fit)
-z<-forecast(fit)
-plot(z)
+HSI_2017 <- forecast(fit, 13)
+HSI_2017 <- HSI_2017$mean
 
+HSI_2017_growth <- HSI_2017/lag(HSI_2017,-1) -1
+
+#Function for percentage calculation 
+percent <- function(x, digits = 2, format = "f", ...) {
+  paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
+}
+
+HSI_2017_growth <- percent(HSI_2017_growth)
 
